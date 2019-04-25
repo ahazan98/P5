7ab8d6d01a47ae792d15d86f81127611989a7f8c
Brandon Ra <brandon.ra97@gmail.com>
2019-04-24 20:32:20 -0400

    Create igraph.h

---
+++ igraph.h
@@ -0,0 +1,37 @@
+#ifndef igraph_h
+#define igraph_h
+#include <cstddef>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <time.h>
+#include <cmath>
+#include <cassert>
+#include <cmath>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Igraph {
+
+
+
+
+    protected:
+
+    private:
+    const vector<string> wl;
+
+    public:
+    Igraph();
+    Igraph(const vector<string> w){
+        wl = w;
+    }
+
+
+};
+
+#endif
