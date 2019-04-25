e53cd56a991eec4cfd17029c32c5d8e2a0c989c4
Brandon Ra <brandon.ra97@gmail.com>
2019-04-24 20:32:15 -0400

    Create wordladder.cpp

---
+++ wordladder.cpp
@@ -0,0 +1,22 @@
+#include "igraph.h"
+
+int main(){
+    string filename;
+    cout << "Enter file name with the words: ";
+    cin >> filename;
+
+    ifstream in;
+    in.open(filename);
+    if(in.fail()){
+        cout << "Error openeing file. \n";
+        exit(1);
+    }
+
+    vector<string> wordlist;
+    string word;
+    while(in >> word){
+        wordlist.push_back(word);
+    }
+
+    // Igraph graph = Igraph(wordList);
+}
